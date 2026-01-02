98a0def70664ccf4cc3b5bc93ee19d02b692f5a3
KonjikiNoYami <zidanalbani97gmail.com>
2026-01-02 17:24:18 +0700

    final v3

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import config from "./utils/env";
-import { PrismaClient } from "../dist/generated";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 let prisma: PrismaClient;
