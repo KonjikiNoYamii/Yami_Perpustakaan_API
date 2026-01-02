b53ccfa93ea17fc165188d9a21644005877d0112
KonjikiNoYami <zidanalbani97gmail.com>
2026-01-02 21:38:38 +0700

    v8

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,41 +1,38 @@
-import config  from "../utils/env";
 import swaggerJSDoc from "swagger-jsdoc";
 
-const option: swaggerJSDoc.Options ={
-    definition:{
-        openapi:'3.0.0',
-        info:{
-            title:'PERPUSTAKAAN API YAMI',
-            version:'1.0.0',
-            description:'Dokumnetasi Lengkap Perpustakaan Api',
-            contact:{
-                name:'Backend developer'
-            },
-        },
-        servers:[
-        {
-            url:`http://${config.HOST}:${config.PORT}`,
-            description:'Development'
-        }
-        ],
-        components:{
-            securitySchemes:{
-                bearerAuth:{
-                    type:'http',
-                    scheme:'bearer',
-                    bearerFormat:'JWT'
-                }
-            }
-        },
-        security:[
-            {
-                bearerAuth:[],
-            },
-        ],
+const option: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PERPUSTAKAAN API YAMI",
+      version: "1.0.0",
+      description: "Dokumentasi Lengkap Perpustakaan API",
+      contact: {
+        name: "Backend Developer",
+      },
     },
-    apis:['src/routes/*.ts']
-}
+    servers: [
+      {
+        url: "/",
+        description: "Current Server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["src/routes/*.ts"],
+};
 
-const swaggerSpec = swaggerJSDoc(option)
-
-export default swaggerSpec
+export default swaggerJSDoc(option);
